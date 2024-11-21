2717d89b80a521cbf3fb71576b5c8658e732bb96
暖愛 <reriogongshi@gmail.com>
2024-11-21 18:40:23 +0900

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,13 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const loadingScreen = document.getElementById('loading');
-    if (loadingScreen) {
-        loadingScreen.style.display = 'none';
-    }
-});
-
 window.addEventListener('load', function() {
     const loadingScreen = document.getElementById('loading');
     if (loadingScreen) {
-        loadingScreen.style.display = 'none';
+        setTimeout(function() {
+            loadingScreen.style.display = 'none';
+        }, 3000);
     }
 });
