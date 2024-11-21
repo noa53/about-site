55ea5e60cba41f95cdda6d46fd767e379695bf94
暖愛 <reriogongshi@gmail.com>
2024-11-21 18:18:57 +0900

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    // ローディング画面を非表示にする
+    const loadingScreen = document.getElementById('loading');
+    if (loadingScreen) {
+        loadingScreen.style.display = 'none'; // ローディング画面を隠す
+    }
+});
